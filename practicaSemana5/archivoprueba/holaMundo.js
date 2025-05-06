d40812e79fe43e06174029260a56a149e711918e
Areli Paez <400802@sistemas.frc.utn.edu.ar>
2025-05-06 01:02:43 -0300

    Proyecto con numeros aleatorios

---
+++ practicaSemana5/archivoprueba/holaMundo.js
@@ -0,0 +1,6 @@
+// Hola mundo en Javascript para ejecutar con Nodejs
+console.log('Hola mundo from Nodejs'); 
+
+// document.write("Hola Mundo"); // Notar que esta línea provocaría error 
+                                 //porque no estoy dentro de un html como para escribir contenido en el documento.
+
