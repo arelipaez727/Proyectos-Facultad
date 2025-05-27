f8066b06d31aabbb4369377e5c06aed7b49563fc
Areli <400802@sistemas.frc.utn.edu.ar>
2025-05-27 10:11:10 -0300

    Etapa 4 pag 53

--- pasoApaso2025/dds-frontend/src/App.jsx
+++ pasoApaso2025/dds-frontend/src/App.jsx
@@ -1,11 +1,29 @@
 import "./App.css";
-import {Categorias} from './components/Categorias';
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import {Menu} from "./components/Menu";
+import { Footer } from "./components/Footer";
+import { Inicio } from "./components/Inicio";
+import { Categorias } from "./components/Categorias";
+import { Articulos } from "./components/articulos/Articulos";
+
 function App() {
   return (
     <>
-      <div className="divBody">
-          <Categorias/>
-      </div>
+        <BrowserRouter>
+          <Menu />
+          <div className="divBody">
+            <Routes>
+              <Route path="/inicio" element={<Inicio />} />
+              <Route
+                path="/categorias"
+                element={<Categorias />}
+              />
+              <Route path="*" element={<Navigate to="/inicio" replace />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
   );
 }
--- pasoApaso2025/dds-frontend/src/components/Categorias.jsx
+++ pasoApaso2025/dds-frontend/src/components/Categorias.jsx
@@ -1,29 +1,37 @@
-import arrayCategoria from '../datos-mock/categorias-mock';
+import React, {useState, useEffect} from 'react';
+import { categoriasMockService } from '../services/categorias-mock.service';
 function Categorias() {
-    return (
-      <>
-        <div className="tituloPagina">Categorias</div>
-        <div className="table-responsive">
-           <table className="table table-bordered table-striped">
-            <thead>
-              <tr>
-                <th style={{ width: "40%" }}>IdCategoria</th>
-                <th style={{ width: "60%" }}>Nombre</th>
+  const tituloPagina = 'Categorias';
+  const [categorias, setCategorias] = useState(null);
+  // cargar al montar el componente (solo una vez)
+  useEffect(() => {
+    BuscarCategorias();
+  }, []);
+  async function BuscarCategorias() {
+    let data = await categoriasMockService.Buscar();
+    setCategorias(data);
+  };
+  return (
+    <div>
+      <div className="tituloPagina">{tituloPagina}</div>
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th style={{ width: "40%" }}>IdCategoria</th>
+            <th style={{ width: "60%" }}>Nombre</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categorias &&
+            categorias.map((categoria) => (
+              <tr key={categoria.IdCategoria}>
+                <td>{categoria.IdCategoria}</td>
+                <td>{categoria.Nombre}</td>
               </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Accesorios</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Audio</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </>
-    );
-  }
-  export { Categorias };
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export {Categorias};
